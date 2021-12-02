7f9c806d0a333e6cbac225699753fea5537ca83f
Mynaniag <76659421+Mynaniag@users.noreply.github.com>
2021-12-02 23:48:24 +0300

    Update views.py

--- application/demo/views.py
+++ application/demo/views.py
@@ -16,7 +16,7 @@ async def health(request: web.Request):
 
 async def app_info(request: web.Request):
     return web.Response(
-        body="Hostname: {}. Version: {}".format(
+        body="Hostname: {}. Version: {}. Dockerhub Build: {}".format(
             HOSTNAME,
             VERSION,
             BUILD,
